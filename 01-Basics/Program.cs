1fb33f8eb5e7b3b613ef57ac620e151c47197ea6
Priyanshu Bhatt <bhattpriyanshupcm007@gmail.com>
2026-07-15 19:30:30 +0530

    01Basic - Data types

---
+++ 01-Basics/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Declaration of a variable
+        String firstName;
+        // Initialization of a variable
+        firstName = "John";
+        String lastName = "Doe";
+
+        int age = 22;
+        bool alive = true;
+        char symbol = '@';
+        double height = 182.1;
+        String username = symbol + firstName + lastName + age;
+
+        Console.WriteLine("Your name is " + firstName);
+        Console.WriteLine("Are you alive? " + alive);
+        Console.WriteLine("Your age is " + age);
+        Console.WriteLine("Your height is " + height + " cm");
+        Console.WriteLine("Your Fav Symbol is " + symbol);
+        Console.WriteLine("Your username is " + username);
+
+
+
+        Console.ReadKey();
+    }
+}
