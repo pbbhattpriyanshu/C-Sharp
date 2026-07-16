6a3421c4510d5080edf3c71d74c324b63b4261b9
Priyanshu Bhatt <bhattpriyanshupcm007@gmail.com>
2026-07-16 13:24:45 +0530

    Some String Methods

---
+++ 04-StringMethods/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("String Methods");
+        String fullName = "Piyush bhatt";
+        String number = "123-456-7890";
+
+
+        fullName = fullName.ToUpper();
+        fullName = fullName.ToLower();
+        number = number.Replace("-", "/");
+        String username = fullName.Insert(0, "Mr.");
+        String firstName = fullName.Substring(0, 6);
+        String lastName = fullName.Substring(6);
+
+        Console.WriteLine(fullName);
+        Console.WriteLine(number);
+        Console.WriteLine(username);
+        Console.WriteLine(fullName.Length);
+        Console.WriteLine(firstName);
+        Console.WriteLine(lastName);
+
+        Console.ReadKey();
+    }
+}
