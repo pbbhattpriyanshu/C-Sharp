12b88ee9bff470355be967b388cf8ddd72c7ec7e
Priyanshu Bhatt <bhattpriyanshupcm007@gmail.com>
2026-07-16 19:40:53 +0530

    Add C# project demonstrating functions and methods
    Added 09-Functions.slnx and 09-Functions.csproj targeting .NET 10.0.
    Introduced Program.cs with examples of function definitions, overloading, parameters, and return values.
    Added Params.cs to showcase the use of the params keyword for variable argument methods.

---
+++ 09-Functions/Params.cs
@@ -0,0 +1,22 @@
+﻿class Params
+{
+    public static void Start()
+    {
+        Console.WriteLine("Params keyword");
+        //params keyword allows a method to accept a variable number of arguments
+
+        double total = CheckOut(3.22, 4.55, 5.66, 6.77);
+
+        Console.WriteLine("Total: " + total);
+    }
+    //Checkout function that accepts a variable number of arguments using params keyword
+    static double CheckOut(params double[] prices)
+    {
+        double total = 0;
+        foreach (double price in prices)
+        {
+            total += price;
+        }
+        return total;
+    }
+}
---
+++ 09-Functions/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Params.Start(); // Calling the Start method from Params class
+        Console.WriteLine("Functions/Methods");
+        //Function - A block of code that performs a specific task and can be called multiple times in a program.
+        greet(); // Calling the function
+        greetPerson("John"); // Calling the function with parameter
+        int result = addNumbers(5, 10); // Calling the function with parameters and return value
+        Console.WriteLine("The sum is: " + result);
+
+        // 1. Function Overloading - Defining multiple functions with the same name but different parameters
+        // name + parameters = signature
+        // function must have different signature to be overloaded
+        int result2 = addNumbers(5, 10, 15); // Calling the overloaded function with different parameters
+        Console.WriteLine("The sum is: " + result2);
+    }
+
+    //Function with no parameters and no return value
+    static void greet()
+    {
+        Console.WriteLine("Good Morning!");
+    }
+
+    //Function with parameters and no return value
+    static void greetPerson(string name)
+    {
+        Console.WriteLine("Good Morning, " + name + "!");
+    }
+
+    //Function with parameters and return value
+    static int addNumbers(int a, int b)
+    {
+        return a + b;
+    }
+
+    //Function Overloading - Defining multiple functions with the same name but different parameters
+    static int addNumbers(int a, int b, int c)
+    {
+        return a + b + c;
+    }
+}
