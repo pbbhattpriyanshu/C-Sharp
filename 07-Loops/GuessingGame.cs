ada631872f99ba0217a76d102e03682182b46361
Priyanshu Bhatt <bhattpriyanshupcm007@gmail.com>
2026-07-16 17:28:15 +0530

    Add .NET 10 project with loops and GuessingGame demo
    - Added 07-Loops.csproj targeting .NET 10.0 with nullable enabled
    - Created 07-Loops.slnx referencing the new project
    - Implemented GuessingGame class with replay support
    - Developed Program.cs to showcase for, while, do-while, and nested loops, and to run the GuessingGame

---
+++ 07-Loops/GuessingGame.cs
@@ -0,0 +1,61 @@
+﻿class  GuessingGame
+{
+    public static void Start()
+    {
+        Console.WriteLine("Welcome to the Guessing Game!");
+
+        Random random = new Random();
+        bool playAgain = true;
+        int min = 0;
+        int max = 100;
+        int guess;
+        int number;
+        int guesses;
+        String response;
+
+        while (playAgain)
+        {
+            guess = 0;
+            guesses = 0;
+            number = random.Next(min, max + 1);
+            response = "";
+
+            while (guess != number)
+            {
+                Console.WriteLine("Guess Number b/w " + min + " - " + max + " : ");
+                guess = Convert.ToInt32(Console.ReadLine());
+
+                if (guess > number)
+                {
+                    Console.WriteLine(guess + " is to high");
+                }
+                else if (guess < number)
+                {
+                    Console.WriteLine(guess + " is to low");
+                }
+
+                guesses++;
+            }
+
+            Console.WriteLine("Number: " + number);
+            Console.WriteLine("You WIN");
+            Console.WriteLine("Guesses " + guesses);
+
+            Console.Write("Would you like to plag again (Y/N): ");
+            response = Console.ReadLine();
+            response = response.ToUpper();
+
+            if (response == "Y")
+            {
+                playAgain = true;
+            } else
+            {
+                playAgain = false;
+            }
+        }
+
+        Console.WriteLine("Thanks for Playing! ..... I Guess");
+
+        Console.ReadKey();
+    }
+}
---
+++ 07-Loops/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Loops");
+        //Loops - Repeating a block of code multiple times
+        // For Loop - Repeats a block of code a specific number of times
+
+        for(int i = 0; i <= 10; i++)
+        {
+            Console.WriteLine(i);
+        }
+
+        // While Loop - Repeats a block of code while a condition is true
+
+        Console.WriteLine("Enter your name: ");
+        String name = Console.ReadLine();
+
+        while (name == "")
+        {
+            Console.WriteLine("Please Enter your name");
+            name = Console.ReadLine();
+        }
+
+        Console.WriteLine("Hello " + name);
+
+        //Nested Loops - A loop inside another loop
+        Console.Write("How many rows do you want to print? ");
+        int rows = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("How many columns do you want to print? ");
+        int columns = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("What Symbol do you want to print? ");
+        String symbol = Console.ReadLine();
+
+        Console.WriteLine();
+
+        for (int i = 0; i<rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write(symbol);
+            }
+            Console.WriteLine();
+        }
+
+        // Do While Loop - Repeats a block of code at least once and then continues while a condition is true
+
+        GuessingGame.Start();
+
+    }
+}
