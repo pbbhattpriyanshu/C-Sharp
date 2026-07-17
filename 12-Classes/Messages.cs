c0b2b044951d59b8e4b2bc96b5285566cbeba4fe
Priyanshu Bhatt <bhattpriyanshupcm007@gmail.com>
2026-07-17 09:09:33 +0530

    Add .NET project with Messages class and demo program
    Initialized 12-Classes.csproj targeting .NET 10.0 and created 12-Classes.slnx referencing the project. Added Messages.cs with a static Messages class containing Hello, Goodbye, and Waiting methods. Implemented Program.cs to demonstrate usage of the Messages class and print introductory text about classes.

---
+++ 12-Classes/Messages.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _12_Classes
+{
+    static class Messages
+    {
+        public static void Hello()
+        {
+            Console.WriteLine("Hello! Welcome to the program");
+        }
+
+        public static void Goodbye()
+        {
+            Console.WriteLine("Goodbye! Thank you for using the program");
+        }
+
+        public static void Waiting()
+        {
+            Console.WriteLine("Please wait while the program is loading...");
+        }
+    }
+}
---
+++ 12-Classes/Program.cs
@@ -0,0 +1,19 @@
+﻿using _12_Classes;
+using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Class");
+            //Class - A class is a blueprint for creating objects. It defines the properties and methods that the objects created from the class will have.
+
+            Messages.Hello();
+            Messages.Goodbye();
+            Messages.Waiting();
+
+        }
+    }
+}
