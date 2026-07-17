ec1ece39a8fd509a7d9dded02d5dabce8a1a85ee
Priyanshu Bhatt <bhattpriyanshupcm007@gmail.com>
2026-07-17 08:56:22 +0530

    Multi-Dimensional Array

---
+++ 11-MultidimensionalArray/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Multi-dimensional Array");
+        // Multi-dimensional array - made up of rows and columns
+
+        String[] ford = { "Mustang", "F-150", "Explorer" };
+        String[] bmw = { "X5", "X3", "M3" };
+        String[] chevy = { "Camaro", "Silverado", "Tahoe" };
+
+        String[,] parking =
+        {
+            { "Mustang", "F-150", "Explorer" },
+            { "X5", "X3", "M3" },
+            { "Camaro", "Silverado", "Tahoe" }
+        };
+
+        // Change Explorer to Escape
+        parking[0,2] = "Escape";
+
+        // Display the array
+        foreach(String car in parking)
+        {
+            Console.WriteLine(car);
+        }
+    }
+}
