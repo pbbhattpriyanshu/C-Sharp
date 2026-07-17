e5f764495e7d28a5cf4d05d6e3b638619cdddd24
Priyanshu Bhatt <bhattpriyanshupcm007@gmail.com>
2026-07-17 20:35:39 +0530

    Add C# project demonstrating method overriding
    Created a new .NET 10.0 console project with root namespace "_16_OOPs3" and enabled implicit usings and nullable. Implemented base class `Animal` with virtual `Speak` method, and derived classes `Dog` and `Cat` overriding `Speak`. Main method demonstrates polymorphism by invoking `Speak` on each object. Added solution file referencing the project.

---
+++ 16-OOPs3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace FirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Method overriding - provide a new version of a method inherited from a parent class.
+            //                    inherited method must be : Abstract, virtual or already overriden
+            //                    used with ToString(), polymorphism
+
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+            dog.Speak();
+            cat.Speak();
+
+        }
+    }
+
+    class Animal
+    {
+        public virtual void Speak()
+        {
+            Console.WriteLine("The animal goes brrr");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public override void Speak()
+        {
+            Console.WriteLine("The dog goes woof");
+        }
+    }
+
+    class Cat: Animal
+    {
+        public override void Speak()
+        {
+            Console.WriteLine("The cat goes meow");
+        }
+    }
+}
