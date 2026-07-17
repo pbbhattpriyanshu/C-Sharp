bd53964648a70499d3fd1b3a9440014314e8a384
Priyanshu Bhatt <bhattpriyanshupcm007@gmail.com>
2026-07-17 18:50:47 +0530

    Add .NET project with basic OOP example in C#
    Added 13-Objects.csproj targeting .NET 10.0 and solution file 13-Objects.slnx. Implemented Program.cs with a Human class demonstrating object-oriented principles: public fields, methods, and object instantiation with method calls.

---
+++ 13-Objects/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("Objects");
+            //Objects are instances of classes. They can have properties and methods that define their behavior.
+            Human human1 = new Human();
+            Human human2 = new Human();
+
+            human1.firstName = "John";
+            human1.age = 40;
+
+            human2.firstName = "Jane";
+            human2.age = 30;
+
+            human1.Introduce();
+            human1.Eat();
+            human1.Sleep();
+
+            human2.Introduce();
+            human2.Eat();
+            human2.Sleep();
+        }
+    }
+
+    //Class
+    class Human
+    {
+        public String firstName;
+        public int age;
+
+        //Method
+        public void Introduce()
+        {
+            Console.WriteLine("Hello, my name is " + firstName + " and I am " + age + " years old.");
+        }
+
+        public void Eat()
+        {
+            Console.WriteLine(firstName + " is eating.");
+        }
+
+        public void Sleep()
+        {
+            Console.WriteLine(firstName + " is sleeping.");
+        }
+    }
+}
