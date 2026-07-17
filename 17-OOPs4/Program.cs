0397d3b3942a1dd3f40eaf88fed6f63065a18fd2
Priyanshu Bhatt <bhattpriyanshupcm007@gmail.com>
2026-07-17 20:54:51 +0530

    Add .NET project and demo polymorphism with vehicles
    Added 17-OOPs4.csproj targeting .NET 10.0 and solution file.
    Implemented Program.cs with Vehicle base class and Car, Bicycle, Boat subclasses overriding Go().
    Demonstrated runtime polymorphism by invoking Go() on each instance in an array.

---
+++ 17-OOPs4/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace firstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Polymorphism");
+            // polymorphism - Geek word means "many forms"
+            //                object can be indentified by more than one type
+            //                Ex. A dog is also: Canine, Animal, Organism
+
+            Car car = new Car();
+            Bicycle bicycle = new Bicycle();
+            Boat boat = new Boat();
+
+            Vechicle[] vehicles = { car, bicycle, boat };
+
+            foreach (Vechicle vechicle in vehicles)
+            {
+                vechicle.Go();
+            }
+
+        }
+    }
+
+    class Vechicle
+    {
+        public virtual void Go()
+        {
+            
+        }
+    }
+
+    class Car : Vechicle
+    {
+        public override void Go()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    class Bicycle : Vechicle
+    {
+        public override void Go()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+
+    class Boat : Vechicle
+    {
+        public override void Go()
+        {
+            Console.WriteLine("Boat is moving");
+        }
+    }
+}
